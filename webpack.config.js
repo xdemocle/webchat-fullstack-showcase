c8122b3776bd8e6ead7c55138c2b1f24c74f6070
xdemocle <xdemocle@gmail.com>
2020-05-25 23:59:13 +0200

    adjust scripts; add/remove deps;

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
-import path from 'path'
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+// Do this as the first thing so that any code reading it knows the right env.
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+
+const path = require('path')
+const webpack = require('webpack')
+const WebpackBar = require('webpackbar')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const BitBarProgressPlugin = require('bitbar-webpack-progress-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlPlugin = require('html-webpack-plugin')
+// const RunNodeWebpackPlugin = require('run-node-webpack-plugin')
+
+const isEnvDevelopment = process.env.NODE_ENV === 'development'
+const isEnvProduction = process.env.NODE_ENV !== 'development'
 
 module.exports = {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  entry: './src/index.ts',
+  mode: isEnvDevelopment ? 'development' : 'production',
+  devtool: isEnvDevelopment ? 'eval-source-map' : 'inline-source-map',
+  entry: './src/app/index.tsx',
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
   },
@@ -14,10 +30,77 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
     ],
   },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].[hash].js',
     path: path.resolve(__dirname, 'dist'),
   },
+  // Some libraries import Node modules but don't use them in the browser.
+  // Tell Webpack to provide empty mocks for them so importing them works.
+  node: {
+    module: 'empty',
+    dgram: 'empty',
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    child_process: 'empty',
+  },
+  // Turn off performance hints during development because we don't do any
+  // splitting or minification in interest of speed. These warnings become
+  // cumbersome.
+  performance: {
+    hints: false,
+  },
+  optimization: {
+    minimize: isEnvProduction,
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor',
+          chunks: 'all',
+        },
+      },
+    },
+  },
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+    new WebpackBar(),
+    new BitBarProgressPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new CleanWebpackPlugin({
+      verbose: true,
+      cleanOnceBeforeBuildPatterns: ['./dist'],
+    }),
+    new CopyPlugin({ patterns: [{ from: 'src/assets', to: '' }] }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: isEnvDevelopment ? '[name].css' : '[name].[contenthash].css',
+      chunkFilename: isEnvDevelopment ? '[id].[name].css' : '[id].[name].[contenthash].css',
+    }),
+    new webpack.SourceMapDevToolPlugin({
+      filename: '[name].js.map',
+      exclude: ['vendor.js', 'vendor.[hash].js'],
+    }),
+    new HtmlPlugin({
+      production: isEnvProduction,
+      inject: true,
+      filename: 'index.html',
+      title: 'Docler Webchat',
+      template: path.resolve(__dirname, 'src/app/index.ejs'),
+      favicon: path.resolve(__dirname, 'src/assets/favicon.ico'),
+      cache: isEnvProduction,
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
+    // new RunNodeWebpackPlugin({ scriptToRun: path.resolve(__dirname, 'run-server.js') }),
+  ],
 }
